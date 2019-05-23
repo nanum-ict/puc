31c88e39735ef9a9162c33552bef7450787a9898
kimyh <mk.kimyh@gmail.com>
2019-05-23 17:57:36 +0900

    font 변경 완료

--- inc/dochead.php
+++ inc/dochead.php
@@ -8,6 +8,6 @@
     <link rel="stylesheet" href="/assets/fontello/css/fontello.css">
     <link rel="stylesheet" href="/assets/css/default.css">
     <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
